7ffa51ba8ab41c87916b7ac6af00d74c5e574788
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-12-25 20:11:53 +0100

    index file rendered

--- myApp.js
+++ myApp.js
@@ -3,11 +3,16 @@ let app = express();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
     res.send("Hello Express");
 })
+ */
 
+const indexPath = __dirname + '/views/index.html'
 
+app.get("/", (req, res) => {
+    res.sendFile(indexPath)
+})
 
 
 
