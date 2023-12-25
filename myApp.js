b09346d3b99075e8a937233e382131bb2db59a98
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-12-25 20:30:06 +0100

    static assets and json rendered

--- myApp.js
+++ myApp.js
@@ -8,12 +8,17 @@ console.log("Hello World");
 })
  */
 
+app.use("/public", express.static(__dirname + "/public"))
+
 const indexPath = __dirname + '/views/index.html'
 
 app.get("/", (req, res) => {
     res.sendFile(indexPath)
 })
 
+app.get("/json", (req, res) => {
+    res.send({'message': "Hello json"})
+})
 
 
 
