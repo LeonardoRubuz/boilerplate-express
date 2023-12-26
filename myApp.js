7e1db6a545cf772899a8943431cded612b1665be
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-12-26 10:09:16 +0100

    zdzd

--- myApp.js
+++ myApp.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
 })
 
 app.get("/json", (req, res) => {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({'message': "Hello json".toUpperCase()})
+    if (process.env["MESSAGE_STYLE"] === "uppercase") {
+        res.json({'message': "HELLO JSON"})
     }else{
         res.json({'message': "Hello json"})
     }
